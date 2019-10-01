5078f764e37cd050db5176661025528eafed874d
SergeiLutsev <snlutsev@outlook.com>
2019-09-30 20:44:06 -0400

    Add base entity

--- pet-clinic-data/src/main/java/com/sergeiSpring/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/sergeiSpring/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.sergeiSpring.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/com/sergeiSpring/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/sergeiSpring/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.sergeiSpring.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
