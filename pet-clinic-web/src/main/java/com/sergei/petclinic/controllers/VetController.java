6775a1f2fc13db4e27cfc0f305c805214f32f60a
SergeiLutsev <snlutsev@outlook.com>
2019-10-07 13:45:18 -0400

    update vets list controller and page

--- pet-clinic-web/src/main/java/com/sergei/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/sergei/petclinic/controllers/VetController.java
@@ -1,14 +1,21 @@
 package com.sergei.petclinic.controllers;
 
+import com.sergei.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
 
     @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String vetsList(){
-
+    public String vetsList(Model model){
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
