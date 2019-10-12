db6df59a210d0495571786ccab7b38f43e6077ae
SergeiLutsev <snlutsev@outlook.com>
2019-10-12 09:43:45 -0400

    add speciality model/entity

---
+++ pet-clinic-data/src/main/java/com/sergei/petclinic/models/Speciality.java
@@ -0,0 +1,13 @@
+package com.sergei.petclinic.models;
+
+public class Speciality extends BaseModel {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
