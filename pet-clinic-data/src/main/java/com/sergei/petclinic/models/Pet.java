84bc4403fb8378ebd095b30f1d0e717d79b2e468
SergeiLutsev <snlutsev@outlook.com>
2019-10-16 12:49:51 -0400

    update visit with JPA entity

--- pet-clinic-data/src/main/java/com/sergei/petclinic/models/Pet.java
+++ pet-clinic-data/src/main/java/com/sergei/petclinic/models/Pet.java
@@ -1,10 +1,9 @@
 package com.sergei.petclinic.models;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -19,6 +18,9 @@ public class Pet extends BaseModel {
     @JoinColumn(name = "type_id")
     private PetType petType;
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits=new HashSet<>();
 
     public Owner getOwner() {
         return owner;
@@ -50,5 +52,13 @@ public class Pet extends BaseModel {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
--- pet-clinic-data/src/main/java/com/sergei/petclinic/models/Visit.java
+++ pet-clinic-data/src/main/java/com/sergei/petclinic/models/Visit.java
@@ -1,11 +1,18 @@
 package com.sergei.petclinic.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.LocalDate;
-
+@Entity
+@Table(name="visits")
 public class Visit extends BaseModel {
 
     private LocalDate date;
     private String description;
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
