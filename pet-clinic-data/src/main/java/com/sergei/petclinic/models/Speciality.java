1ed55b8a4263bf09a3b5524be6e8378481ac76c6
SergeiLutsev <snlutsev@outlook.com>
2019-10-16 12:40:40 -0400

    update vet with JPA entity

--- pet-clinic-data/src/main/java/com/sergei/petclinic/models/Speciality.java
+++ pet-clinic-data/src/main/java/com/sergei/petclinic/models/Speciality.java
@@ -1,5 +1,10 @@
 package com.sergei.petclinic.models;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseModel {
     private String description;
 
--- pet-clinic-data/src/main/java/com/sergei/petclinic/models/Vet.java
+++ pet-clinic-data/src/main/java/com/sergei/petclinic/models/Vet.java
@@ -1,10 +1,14 @@
 package com.sergei.petclinic.models;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
-
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
-
+    @ManyToMany
+    @JoinTable(name = "vet_specialitiy", joinColumns = @JoinColumn(name="vet_id"),
+                inverseJoinColumns = @JoinColumn(name="specialitiy_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
